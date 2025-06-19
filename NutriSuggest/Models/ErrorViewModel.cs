4ba924e7203c06a66390702c67cc4738e39f7f5d
harmeet-singh-project <its.harmeet.singh@outlook.com>
2025-06-19 19:30:34 -0400

    Add project files.

---
+++ NutriSuggest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutriSuggest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
